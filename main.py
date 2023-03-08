27a2e1742cd73721dd8ab6357d9452a1ffec7b65
Hannah Wilson <127059267+19227JHC@users.noreply.github.com>
2023-03-08 20:50:12 +0000

    Added title and instructions

--- main.py
+++ main.py
@@ -6,3 +6,5 @@
   
   TIP: Use assessment guide to help guide you through this Internal
 '''
+print("Splatoon Quiz")
+print("Welcome to the Splatoon Quiz!\nThis quiz will test your knowledge of the game Splatoon.\nTo play, use the letters 'a', 'b', or 'c' to select your answer to each question. \nGood luck!\n")
