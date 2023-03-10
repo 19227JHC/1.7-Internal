baf8eab8d7e8fd17e6e5cba7a93df00fb154a93d
Hannah Wilson <127059267+19227JHC@users.noreply.github.com>
2023-03-10 00:28:35 +0000

    Drafting how to do this. Trying a dictionary I guess

--- main.py
+++ main.py
@@ -6,5 +6,18 @@
   
   TIP: Use assessment guide to help guide you through this Internal
 '''
-print("Splatoon Quiz")
-print("Welcome to the Splatoon Quiz!\nThis quiz will test your knowledge of the game Splatoon.\nTo play, use the letters 'a', 'b', or 'c' to select your answer to each question. \nGood luck!\n")
+print("|---Splatoon Quiz---|")
+print("Welcome to the Splatoon Quiz!\nThis quiz will test your knowledge of the game Splatoon.\nTo play, use the letters 'a', 'b', or 'c' to select your answer to each question.\nGood luck!\n")
+
+# define questions and answers in a dictionary
+questions = [
+    {
+        "question": "Which of these idol groups hosted Inkopolis News in splatoon 1?",
+        "options": {
+            "a": "Off The Hook",
+            "b": "Squid Sisters",
+            "c": "Deepcut"
+        },
+        "answer": "b"
+    },
+]
