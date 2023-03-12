ecf386324c661b6d981568b10ff40559b2853bc6
Hannah Wilson <127059267+19227JHC@users.noreply.github.com>
2023-03-12 22:54:27 +0000

    Fixed the issue

--- main.py
+++ main.py
@@ -10,54 +10,51 @@ print("|---Splatoon Quiz---|")
 print("Welcome to the Splatoon Quiz!\nThis quiz will test your knowledge of the game Splatoon.\nTo play, use the letters 'a', 'b', or 'c' to select your answer to each question.\nGood luck!\n")
 
 # define the questions and their answers
-questions = [
-      {
-        "question": "Which of these idol groups hosted Inkopolis News in splatoon 1?",
-        "choices": ["A.Off The Hook", "B. Squid Sisters", "C. Deep Cut"],
+questions = [    {        "question": "Which of these idol groups hosted Inkopolis News in Splatoon 1?",        "choices": ["A.Off The Hook", "B. Squid Sisters", "C. Deep Cut"],
         "answer": "B"
-    }
+    },
     {
         "question": "Where does the Octo Expansion take place?",
         "choices": ["A.Deepsea Metro", "B.Octo Canyon", "C.Alterna"],
         "answer": "A"
-    }
+    },
     {
         "question": "What is sanitized ink made of?",
         "choices": ["A.Sanitizer products", "B.Human remnants", "C.Blended up octarians"],
         "answer": "C"
-    }
+    },
     {
-        "question": "What type of Salmonid is Little Buddy",
+        "question": "What type of Salmonid is Little Buddy?",
         "choices": ["A.Cohock", "B.Chum.", "C.Smallfry"],
         "answer": "C"
-    }
+    },
     {
-        "question": "Which of the following is not a playable mode in splatoon 3",
+        "question": "Which of the following is not a playable mode in Splatoon 3?",
         "choices": ["A.Inkstrike", "B.Splat Zones", "C.Rainmaker"],
         "answer": "A"
-    }
+    },
     {
-        "question": "Who is 'The Ursine Anomaly #03'",
+        "question": "Who is 'The Ursine Anomaly #03'?",
         "choices": ["A.lil Judd", "B.Mr. Grizz", "C.Mr. Coco"],
         "answer": "B"
-    }
+    },
     {
         "question":"What is the name of the AI in Alterna?",
         "choices": ["A.C.Q. Cumber", "B.Commander Tartar", "C.O.R.C.A."],
         "answer": "C"
-    }
+    },
     {
-        "question": "Who is the leader of the octarians army?",
-        "choices": ["A.DJ Octavio", "B.Captain cuttlefish", "C.Inner Agent 3"],
+        "question": "Who is the leader of the Octarian army?",
+        "choices": ["A.DJ Octavio", "B.Captain Cuttlefish", "C.Inner Agent 3"],
         "answer": "A"
-    }
+    },
     {
-        "question": "The fuzzy ooze is made up of liquid crystals, Mr Grizzs fur and ___",
+        "question": "The Fuzzy Ooze is made up of liquid crystals, Mr. Grizz's fur, and ___",
         "choices": ["A.Salmonids", "B.Golden Eggs", "C.Human remnant"],
         "answer": "B"
-    }
+    },
     {
-        "question": "How many Denizens of the deep are there in the deepsea metro",
+        "question": "How many Denizens of the Deep are there in the Deepsea Metro?",
         "choices": ["A.5", "B.13", "C.9"],
         "answer": "C"
     }
